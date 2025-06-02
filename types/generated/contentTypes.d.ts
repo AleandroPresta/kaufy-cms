cb22574dd83440aa403f73484d73a23044a8c881
Aleandro Presta <37838171+AleandroPresta@users.noreply.github.com>
2025-06-02 14:55:51 +0200

    Remove 'description' and 'image_url' attributes from product schema and TypeScript definitions

--- types/generated/contentTypes.d.ts
+++ types/generated/contentTypes.d.ts
@@ -448,8 +448,6 @@ export interface ApiProductProduct extends Struct.CollectionTypeSchema {
     createdAt: Schema.Attribute.DateTime;
     createdBy: Schema.Attribute.Relation<'oneToOne', 'admin::user'> &
       Schema.Attribute.Private;
-    description: Schema.Attribute.Blocks;
-    image_url: Schema.Attribute.String;
     locale: Schema.Attribute.String & Schema.Attribute.Private;
     localizations: Schema.Attribute.Relation<
       'oneToMany',
