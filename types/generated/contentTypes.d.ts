f8756936737d2dd67f2c033f502a7cc32c63522d
Aleandro Presta <37838171+AleandroPresta@users.noreply.github.com>
2025-06-02 14:45:31 +0200

    Add 'image_url' attribute to product schema and TypeScript definitions

--- types/generated/contentTypes.d.ts
+++ types/generated/contentTypes.d.ts
@@ -449,6 +449,7 @@ export interface ApiProductProduct extends Struct.CollectionTypeSchema {
     createdBy: Schema.Attribute.Relation<'oneToOne', 'admin::user'> &
       Schema.Attribute.Private;
     description: Schema.Attribute.Blocks;
+    image_url: Schema.Attribute.String;
     locale: Schema.Attribute.String & Schema.Attribute.Private;
     localizations: Schema.Attribute.Relation<
       'oneToMany',
